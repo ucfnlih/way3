ae5d96eb9742260a82a7fb0c3a9cd5805208f91b
ucfnlih <ucfnlih@ucl.ac.uk>
2022-11-02 00:33:14 +0000

    change

--- way3/way3.R
+++ way3/way3.R
@@ -1,29 +1,23 @@
-520+520
-5200+5200
+library(sf)
 library(tidyverse)
-listfile2<-dir_info("allfiles/") %>%
-  filter(str_detect(path, "wc2.1_5m_tmax_ACCESS-CM2_ssp585_2081-2100.tif")) %>%
-  dplyr::select(path)%>%
-  pull()
-worldclimtemp1 <- listfile1 %>%
-  terra::rast()
-worldclimtemp2 <- listfile2 %>%
-  terra::rast()
-ssp126 <- c("ssp126a","ssp126b","ssp126c","ssp126d","ssp126e","ssp126f","ssp126g","ssp126h","ssp126i","ssp126j","ssp126k","ssp126l")
-ssp585 <- c("ssp585a","ssp585b","ssp585c","ssp585d","ssp585e","ssp585f","ssp585g","ssp585h","ssp585i","ssp585j","ssp585k","ssp585l")
-names(worldclimtemp1) <- ssp126
-names(worldclimtemp2) <- ssp585
-worldclimtemp1
-worldclimtemp2
-AUcitytemp1<- terra::extract(worldclimtemp1, samples)
-AUcitytemp2<- terra::extract(worldclimtemp2, samples)
-Aucitytemp1 <- AUcitytemp1 %>% 
-  as_tibble()%>% 
-  add_column(Site = site, .before = "ssp126a")
-Aucitytemp2 <- AUcitytemp2 %>% 
-  as_tibble()%>% 
-  add_column(Site = site, .before = "ssp585a")
-Aucitytemp1_ssp_select<-Aucitytemp1[,c(3:14)]
-Aucitytemp1_ssp_max<-pmax(Aucitytemp1_ssp_select$ssp126a,Aucitytemp1_ssp_select$ssp126b,Aucitytemp1_ssp_select$ssp126c,Aucitytemp1_ssp_select$ssp126d,Aucitytemp1_ssp_select$ssp126e,Aucitytemp1_ssp_select$ssp126f,Aucitytemp1_ssp_select$ssp126g,Aucitytemp1_ssp_select$ssp126h,Aucitytemp1_ssp_select$ssp126i,Aucitytemp1_ssp_select$ssp126j,Aucitytemp1_ssp_select$ssp126k,Aucitytemp1_ssp_select$ssp126l)
-Aucitytemp1_ssp_select<-Aucitytemp1[,c(3:14)]
+library(here)
+library(janitor)
+library(countrycode)
+library(usethis)
+shape <- st_read("C:/Users/WD/Desktop/GIS/WEEK4_HW/WEEK4_Homework/World_Countries_(Generalized)/World_Countries__Generalized_.shp")
+HDI <- read_csv(here::here("HDR21-22_Composite_indices_complete_time_series.csv"),
+                locale = locale(encoding = "latin1"),
+                na = " ", skip=0)
+## Column names
+HDIcols<- HDI %>%
+  clean_names()%>%
+  select(iso3, country, gii_2019, gii_2010)%>%
+  mutate(difference=gii_2019-gii_2010)%>%
+  mutate(iso_code=countrycode(country, origin = 'country.name', destination = 'iso2c'))%>%
+  mutate(iso_code2=countrycode(iso3, origin ='iso3c', destination = 'iso2c'))
+Join_HDI <- shape %>% 
+  clean_names() %>%
+  left_join(., 
+            HDIcols,
+            by = c("iso" = "iso_code"))
 
